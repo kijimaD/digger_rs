17991c4efbdac5a2638277531c54b6ef811c998f
kijimaD <norimaking777@gmail.com>
2022-04-12 22:51:12 +0900

    refactor: remove block

--- src/melee_combat_system.rs
+++ src/melee_combat_system.rs
@@ -101,29 +101,27 @@ impl<'a> System<'a> for MeleeCombatSystem {
     }
 }
 
-// TODO: systemにしたい
+// TODO: systemにしたいが、state切り替えがうまくいかず諦めた
 pub fn invoke_battle(ecs: &mut World) {
-    {
-        let mut wants_encounter = ecs.write_storage::<WantsToEncounter>();
-        let mut battlelog = ecs.write_resource::<BattleLog>();
-        let mut battle = ecs.write_storage::<Battle>();
+    let mut wants_encounter = ecs.write_storage::<WantsToEncounter>();
+    let mut battlelog = ecs.write_resource::<BattleLog>();
+    let mut battle = ecs.write_storage::<Battle>();
 
-        for wants_encounter in (&wants_encounter).join().take(1) {
-            // 最初のwants_encounterだけ処理する
-            let mut runstate = ecs.write_resource::<RunState>();
-            *runstate = RunState::BattleEncounter;
-            battle
-                .insert(
-                    wants_encounter.monster,
-                    Battle {
-                        monster: wants_encounter.monster,
-                    },
-                )
-                .expect("Unable to insert encounter");
+    for wants_encounter in (&wants_encounter).join().take(1) {
+        // 最初のwants_encounterだけ処理する
+        let mut runstate = ecs.write_resource::<RunState>();
+        *runstate = RunState::BattleEncounter;
+        battle
+            .insert(
+                wants_encounter.monster,
+                Battle {
+                    monster: wants_encounter.monster,
+                },
+            )
+            .expect("Unable to insert encounter");
 
-            battlelog.entries = vec![];
-            battlelog.entries.push(format!("Monster appearing"));
-        }
-        wants_encounter.clear();
+        battlelog.entries = vec![];
+        battlelog.entries.push(format!("Monster appearing"));
     }
+    wants_encounter.clear();
 }
