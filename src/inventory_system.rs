8a3a20ca09ea55fb05b9ea169030ab58f39b5be5
kijimaD <norimaking777@gmail.com>
2022-04-17 20:03:09 +0900

    fix: item disappear bug

--- src/inventory_system.rs
+++ src/inventory_system.rs
@@ -118,7 +118,7 @@ impl<'a> System<'a> for ItemUseSystem {
                     for item in to_unequip.iter() {
                         equipped.remove(*item);
                         backpack
-                            .insert(*item, InBackpack { owner: target })
+                            .insert(*item, InBackpack { owner: *player_entity })
                             .expect("Unable to insert backpack entry");
                     }
 
@@ -260,16 +260,19 @@ impl<'a> System<'a> for ItemRemoveSystem {
         WriteStorage<'a, WantsToRemoveItem>,
         WriteStorage<'a, Equipped>,
         WriteStorage<'a, InBackpack>,
+        WriteExpect<'a, GameLog>,
+        ReadStorage<'a, Name>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (entities, mut wants_remove, mut equipped, mut backpack) = data;
+        let (entities, mut wants_remove, mut equipped, mut backpack, mut gamelog, names) = data;
 
         for (entity, to_remove) in (&entities, &wants_remove).join() {
             equipped.remove(to_remove.item);
             backpack
                 .insert(to_remove.item, InBackpack { owner: entity })
                 .expect("Unable to insert backpack");
+            gamelog.entries.push(format!("You unequip {}.", names.get(to_remove.item).unwrap().name));
         }
 
         wants_remove.clear();
