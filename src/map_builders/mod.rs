a85c5ed581a3a12ad9fb844b79304c803bf17792
kijimaD <norimaking777@gmail.com>
2022-05-05 14:50:35 +0900

    feat: bsp functions(chapter25)

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -17,6 +17,10 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    // Box::new(SimpleMapBuilder::new(new_depth))
-    Box::new(BspDungeonBuilder::new(new_depth))
+    let mut rng = rltk::RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 2);
+    match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth))
+    }
 }
