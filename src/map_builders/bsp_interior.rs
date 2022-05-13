fc64508db46f6f41b9754905fe6852e7ebca3be6
kijimaD <norimaking777@gmail.com>
2022-05-13 23:23:08 +0900

    refactor: bsp_interior

--- src/map_builders/bsp_interior.rs
+++ src/map_builders/bsp_interior.rs
@@ -1,5 +1,6 @@
 use super::{
-    apply_room_to_map, spawner, Map, MapBuilder, Position, Rect, TileType, SHOW_MAPGEN_VISUALIZER,
+    apply_room_to_map, draw_corridor, spawner, BuilderMap, InitialMapBuilder, Map, Position, Rect,
+    TileType, SHOW_MAPGEN_VISUALIZER,
 };
 use rltk::RandomNumberGenerator;
 use specs::prelude::*;
@@ -7,105 +8,60 @@ use specs::prelude::*;
 const MIN_ROOM_SIZE: i32 = 8;
 
 pub struct BspInteriorBuilder {
-    map: Map,
-    starting_position: Position,
-    depth: i32,
-    rooms: Vec<Rect>,
-    history: Vec<Map>,
     rects: Vec<Rect>,
 }
 
-impl MapBuilder for BspInteriorBuilder {
-    fn get_map(&self) -> Map {
-        self.map.clone()
-    }
-
-    fn get_starting_position(&self) -> Position {
-        self.starting_position.clone()
-    }
-
-    fn get_snapshot_history(&self) -> Vec<Map> {
-        self.history.clone()
-    }
-
-    fn build_map(&mut self) {
-        self.build();
-    }
-
-    fn spawn_entities(&mut self, ecs: &mut World) {
-        for room in self.rooms.iter().skip(1) {
-            spawner::spawn_room(ecs, room, self.depth);
-        }
-    }
-
-    fn take_snapshot(&mut self) {
-        if SHOW_MAPGEN_VISUALIZER {
-            let mut snapshot = self.map.clone();
-            for v in snapshot.revealed_tiles.iter_mut() {
-                *v = true;
-            }
-            self.history.push(snapshot);
-        }
+impl InitialMapBuilder for BspInteriorBuilder {
+    fn build_map(&mut self, rng: &mut rltk::RandomNumberGenerator, build_data: &mut BuilderMap) {
+        self.build(rng, build_data);
     }
 }
 
 impl BspInteriorBuilder {
-    pub fn new(new_depth: i32) -> BspInteriorBuilder {
-        BspInteriorBuilder {
-            map: Map::new(new_depth),
-            starting_position: Position { x: 0, y: 0 },
-            depth: new_depth,
-            rooms: Vec::new(),
-            history: Vec::new(),
-            rects: Vec::new(),
-        }
+    pub fn new(new_depth: i32) -> Box<BspInteriorBuilder> {
+        Box::new(BspInteriorBuilder { rects: Vec::new() })
     }
 
-    fn build(&mut self) {
-        let mut rng = RandomNumberGenerator::new();
+    fn build(&mut self, rng: &mut RandomNumberGenerator, build_data: &mut BuilderMap) {
+        let mut rooms: Vec<Rect> = Vec::new();
 
         self.rects.clear();
-        self.rects.push(Rect::new(1, 1, self.map.width - 2, self.map.height - 2));
+        self.rects.push(Rect::new(1, 1, build_data.map.width - 2, build_data.map.height - 2));
         let first_room = self.rects[0];
         self.add_subrects(first_room, &mut rng);
 
         let rooms = self.rects.clone();
         for r in rooms.iter() {
             let room = *r;
-            self.rooms.push(room);
+            rooms.push(room);
             for y in room.y1..room.y2 {
                 for x in room.x1..room.x2 {
-                    let idx = self.map.xy_idx(x, y);
-                    if idx > 0 && idx < ((self.map.width * self.map.height) - 1) as usize {
-                        self.map.tiles[idx] = TileType::Floor;
+                    let idx = build_data.map.xy_idx(x, y);
+                    if idx > 0
+                        && idx < ((build_data.map.width * build_data.map.height) - 1) as usize
+                    {
+                        build_data.map.tiles[idx] = TileType::Floor;
                     }
                 }
             }
-            self.take_snapshot();
+            build_data.take_snapshot();
         }
 
-        // Player start position
-        let start = self.rooms[0].center();
-        self.starting_position = Position { x: start.0, y: start.1 };
-
         // Corridor
-        for i in 0..self.rooms.len() - 1 {
-            let room = self.rooms[i];
-            let next_room = self.rooms[i + 1];
+        for i in 0..rooms.len() - 1 {
+            let room = rooms[i];
+            let next_room = rooms[i + 1];
             let start_x = room.x1 + (rng.roll_dice(1, i32::abs(room.x1 - room.x2)) - 1);
             let start_y = room.y1 + (rng.roll_dice(1, i32::abs(room.y1 - room.y2)) - 1);
             let end_x =
                 next_room.x1 + (rng.roll_dice(1, i32::abs(next_room.x1 - next_room.x2)) - 1);
             let end_y =
                 next_room.y1 + (rng.roll_dice(1, i32::abs(next_room.y1 - next_room.y2)) - 1);
-            self.draw_corridor(start_x, start_y, end_x, end_y);
-            self.take_snapshot();
+            draw_corridor(&mut build_data.map, start_x, start_y, end_x, end_y);
+            build_data.take_snapshot();
         }
 
-        // Stairs
-        let stairs = self.rooms[self.rooms.len() - 1].center();
-        let stairs_idx = self.map.xy_idx(stairs.0, stairs.1);
-        self.map.tiles[stairs_idx] = TileType::DownStairs;
+        build_data.rooms = Some(rooms);
     }
 
     // #############
@@ -164,26 +120,6 @@ impl BspInteriorBuilder {
             if half_height > MIN_ROOM_SIZE {
                 self.add_subrects(v2, rng);
             }
-        }
-    }
-
-    fn draw_corridor(&mut self, x1: i32, y1: i32, x2: i32, y2: i32) {
-        let mut x = x1;
-        let mut y = y1;
-
-        while x != x2 || y != y2 {
-            if x < x2 {
-                x += 1;
-            } else if x > x2 {
-                x -= 1;
-            } else if y < y2 {
-                y += 1;
-            } else if y > y2 {
-                y -= 1;
-            }
-
-            let idx = self.map.xy_idx(x, y);
-            self.map.tiles[idx] = TileType::Floor;
         }
     }
 }
