411cc8c57d394e21be8fc4949354bc71b3c0d68e
kijimaD <norimaking777@gmail.com>
2022-05-15 11:05:09 +0900

    style: fmt

--- src/map_builders/room_corner_rounding.rs
+++ src/map_builders/room_corner_rounding.rs
@@ -1,4 +1,4 @@
-use super::{MetaMapBuilder, BuilderMap, TileType, Rect};
+use super::{BuilderMap, MetaMapBuilder, Rect, TileType};
 use rltk::RandomNumberGenerator;
 
 pub struct RoomCornerRounder {}
@@ -12,7 +12,7 @@ impl MetaMapBuilder for RoomCornerRounder {
 impl RoomCornerRounder {
     #[allow(dead_code)]
     pub fn new() -> Box<RoomCornerRounder> {
-        Box::new(RoomCornerRounder{})
+        Box::new(RoomCornerRounder {})
     }
 
     fn fill_if_corner(&mut self, x: i32, y: i32, build_data: &mut BuilderMap) {
@@ -20,10 +20,18 @@ impl RoomCornerRounder {
         let h = build_data.map.height;
         let idx = build_data.map.xy_idx(x, y);
         let mut neighbor_walls = 0;
-        if x > 0 && build_data.map.tiles[idx-1] == TileType::Wall { neighbor_walls += 1; }
-        if y > 0 && build_data.map.tiles[idx-w as usize] == TileType::Wall { neighbor_walls += 1; }
-        if x < w-2 && build_data.map.tiles[idx+1] == TileType::Wall { neighbor_walls += 1; }
-        if y < h-2 && build_data.map.tiles[idx+w as usize] == TileType::Wall { neighbor_walls += 1; }
+        if x > 0 && build_data.map.tiles[idx - 1] == TileType::Wall {
+            neighbor_walls += 1;
+        }
+        if y > 0 && build_data.map.tiles[idx - w as usize] == TileType::Wall {
+            neighbor_walls += 1;
+        }
+        if x < w - 2 && build_data.map.tiles[idx + 1] == TileType::Wall {
+            neighbor_walls += 1;
+        }
+        if y < h - 2 && build_data.map.tiles[idx + w as usize] == TileType::Wall {
+            neighbor_walls += 1;
+        }
 
         if neighbor_walls == 2 {
             build_data.map.tiles[idx] = TileType::Wall;
@@ -39,9 +47,9 @@ impl RoomCornerRounder {
         }
 
         for room in rooms.iter() {
-            self.fill_if_corner(room.x1+1, room.y1+1, build_data);
-            self.fill_if_corner(room.x2, room.y1+1, build_data);
-            self.fill_if_corner(room.x1+1, room.y2, build_data);
+            self.fill_if_corner(room.x1 + 1, room.y1 + 1, build_data);
+            self.fill_if_corner(room.x2, room.y1 + 1, build_data);
+            self.fill_if_corner(room.x1 + 1, room.y2, build_data);
             self.fill_if_corner(room.x2, room.y2, build_data);
 
             build_data.take_snapshot();
--- src/map_builders/room_exploder.rs
+++ src/map_builders/room_exploder.rs
@@ -1,4 +1,4 @@
-use super::{MetaMapBuilder, BuilderMap, TileType, paint, Symmetry, Rect};
+use super::{paint, BuilderMap, MetaMapBuilder, Rect, Symmetry, TileType};
 use rltk::RandomNumberGenerator;
 
 pub struct RoomExploder {}
@@ -12,7 +12,7 @@ impl MetaMapBuilder for RoomExploder {
 impl RoomExploder {
     #[allow(dead_code)]
     pub fn new() -> Box<RoomExploder> {
-        Box::new(RoomExploder{})
+        Box::new(RoomExploder {})
     }
 
     fn build(&mut self, rng: &mut RandomNumberGenerator, build_data: &mut BuilderMap) {
@@ -25,7 +25,7 @@ impl RoomExploder {
 
         for room in rooms.iter() {
             let start = room.center();
-            let n_diggers = rng.roll_dice(1, 20)-5;
+            let n_diggers = rng.roll_dice(1, 20) - 5;
             if n_diggers > 0 {
                 for _i in 0..n_diggers {
                     let mut drunk_x = start.0;
@@ -36,7 +36,7 @@ impl RoomExploder {
 
                     while drunk_life > 0 {
                         let drunk_idx = build_data.map.xy_idx(drunk_x, drunk_y);
-                        if build_data.map.tiles[drunk_idx] == TileType::Wall{
+                        if build_data.map.tiles[drunk_idx] == TileType::Wall {
                             did_something = true;
                         }
                         paint(&mut build_data.map, Symmetry::None, 1, drunk_x, drunk_y);
@@ -44,10 +44,26 @@ impl RoomExploder {
 
                         let stagger_direction = rng.roll_dice(1, 4);
                         match stagger_direction {
-                            1 => { if drunk_x > 2 { drunk_x -= 1; } }
-                            2 => { if drunk_x < build_data.map.width-2 { drunk_x += 1; } }
-                            3 => { if drunk_y > 2 { drunk_y -= 1; } }
-                            _ => { if drunk_y < build_data.map.height-2 { drunk_y += 1; } }
+                            1 => {
+                                if drunk_x > 2 {
+                                    drunk_x -= 1;
+                                }
+                            }
+                            2 => {
+                                if drunk_x < build_data.map.width - 2 {
+                                    drunk_x += 1;
+                                }
+                            }
+                            3 => {
+                                if drunk_y > 2 {
+                                    drunk_y -= 1;
+                                }
+                            }
+                            _ => {
+                                if drunk_y < build_data.map.height - 2 {
+                                    drunk_y += 1;
+                                }
+                            }
                         }
 
                         drunk_life -= 1;
