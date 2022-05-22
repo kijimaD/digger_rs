714912308efcfd71e10973d7edb2ae6c8cee2e0d
kijimaD <norimaking777@gmail.com>
2022-05-22 12:37:35 +0900

    feat: spawn mob

--- src/raws/rawmaster.rs
+++ src/raws/rawmaster.rs
@@ -10,11 +10,16 @@ pub enum SpawnType {
 pub struct RawMaster {
     raws: Raws,
     item_index: HashMap<String, usize>,
+    mob_index: HashMap<String, usize>,
 }
 
 impl RawMaster {
     pub fn empty() -> RawMaster {
-        RawMaster { raws: Raws { items: Vec::new() }, item_index: HashMap::new() }
+        RawMaster {
+            raws: Raws { items: Vec::new(), mobs: Vec::new() },
+            item_index: HashMap::new(),
+            mob_index: HashMap::new(),
+        }
     }
 
     pub fn load(&mut self, raws: Raws) {
@@ -22,6 +27,9 @@ impl RawMaster {
         self.item_index = HashMap::new();
         for (i, item) in self.raws.items.iter().enumerate() {
             self.item_index.insert(item.name.clone(), i);
+        }
+        for (i, mob) in self.raws.mobs.iter().enumerate() {
+            self.mob_index.insert(mob.name.clone(), i);
         }
     }
 }
@@ -37,19 +45,12 @@ pub fn spawn_named_item(
 
         let mut eb = new_entity;
 
-        match pos {
-            SpawnType::AtPosition { x, y } => {
-                eb = eb.with(Position { x, y });
-            }
-        }
+        // Spawn in the specified location
+        eb = spawn_position(pos, eb);
 
+        // Renderable
         if let Some(renderable) = &item_template.renderable {
-            eb = eb.with(crate::components::Renderable {
-                glyph: rltk::to_cp437(renderable.glyph.chars().next().unwrap()),
-                fg: rltk::RGB::from_hex(&renderable.fg).expect("Invalid RGB"),
-                bg: rltk::RGB::from_hex(&renderable.bg).expect("Invalid RGB"),
-                render_order: renderable.order,
-            })
+            eb = eb.with(get_renderable_component(renderable));
         }
 
         eb = eb.with(Name { name: item_template.name.clone() });
@@ -92,4 +93,69 @@ pub fn spawn_named_item(
         return Some(eb.build());
     }
     None
+}
+
+pub fn spawn_named_mob(
+    raws: &RawMaster,
+    new_entity: EntityBuilder,
+    key: &str,
+    pos: SpawnType,
+) -> Option<Entity> {
+    if raws.mob_index.contains_key(key) {
+        let mob_template = &raws.raws.mobs[raws.mob_index[key]];
+
+        let mut eb = new_entity;
+
+        // Spawn in the specified location
+        eb = spawn_position(pos, eb);
+
+        // Renderable
+        if let Some(renderable) = &mob_template.renderable {
+            eb = eb.with(get_renderable_component(renderable));
+        }
+
+        eb = eb.with(Name { name: mob_template.name.clone() });
+
+        eb = eb.with(Monster {});
+        if mob_template.blocks_tile {
+            eb = eb.with(BlocksTile {});
+        }
+        eb = eb.with(CombatStats {
+            max_hp: mob_template.stats.max_hp,
+            hp: mob_template.stats.hp,
+            power: mob_template.stats.power,
+            defense: mob_template.stats.defense,
+        });
+        eb = eb.with(Viewshed {
+            visible_tiles: Vec::new(),
+            range: mob_template.vision_range,
+            dirty: true,
+        });
+
+        return Some(eb.build());
+    }
+    None
+}
+
+fn spawn_position(pos: SpawnType, new_entity: EntityBuilder) -> EntityBuilder {
+    let mut eb = new_entity;
+
+    match pos {
+        SpawnType::AtPosition { x, y } => {
+            eb = eb.with(Position { x, y });
+        }
+    }
+
+    eb
+}
+
+fn get_renderable_component(
+    renderable: &super::item_structs::Renderable,
+) -> crate::components::Renderable {
+    crate::components::Renderable {
+        glyph: rltk::to_cp437(renderable.glyph.chars().next().unwrap()),
+        fg: rltk::RGB::from_hex(&renderable.fg).expect("Invalid RGB"),
+        bg: rltk::RGB::from_hex(&renderable.bg).expect("Invalid RGB"),
+        render_order: renderable.order,
+    }
 }
