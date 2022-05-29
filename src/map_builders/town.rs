86475098fea730bbc52b35428b4c09c7c45e0edb
kijimaD <norimaking777@gmail.com>
2022-05-29 21:55:21 +0900

    feat: building outline

--- src/map_builders/town.rs
+++ src/map_builders/town.rs
@@ -41,6 +41,7 @@ impl TownBuilder {
         build_data.take_snapshot();
 
         let (mut available_building_tiles, wall_gap_y) = self.town_walls(rng, build_data);
+        let mut buildings = self.buildings(rng, build_data, &mut available_building_tiles);
 
         build_data.starting_position = Some(Position { x: 30, y: 30 })
     }
@@ -126,5 +127,69 @@ impl TownBuilder {
         build_data.take_snapshot();
 
         (available_building_tiles, wall_gap_y)
+    }
+
+    fn buildings(&mut self,
+                 rng: &mut rltk::RandomNumberGenerator,
+                 build_data: &mut BuilderMap,
+                 available_building_tiles: &mut HashSet<usize>)
+                 -> Vec<(i32, i32, i32, i32)>
+    {
+        let mut buildings: Vec<(i32, i32, i32, i32)> = Vec::new();
+        let mut n_buildings = 0;
+        while n_buildings < 12 {
+            let bx = rng.roll_dice(1, build_data.map.width - 32) + 30;
+            let by = rng.roll_dice(1, build_data.map.height)-2;
+            let bw = rng.roll_dice(1, 8)+4;
+            let bh = rng.roll_dice(1, 8)+4;
+            let mut possible = true;
+            for y in by .. by+bh {
+                for x in bx .. bx+bw {
+                    if x < 0 || x > build_data.width-1 || y < 0 || y > build_data.height-1 {
+                        possible = false;
+                    } else {
+                        let idx = build_data.map.xy_idx(x, y);
+                        if !available_building_tiles.contains(&idx) { possible = false; }
+                    }
+                }
+            }
+            if possible {
+                n_buildings += 1;
+                buildings.push((bx, by, bw, bh));
+                for y in by .. by+bh {
+                    for x in bx .. bx+bh {
+                        let idx = build_data.map.xy_idx(x, y);
+                        build_data.map.tiles[idx] = TileType::WoodFloor;
+                        available_building_tiles.remove(&idx);
+                        available_building_tiles.remove(&(idx+1));
+                        available_building_tiles.remove(&(idx+build_data.width as usize));
+                        available_building_tiles.remove(&(idx-1));
+                        available_building_tiles.remove(&(idx-build_data.width as usize));
+                    }
+                }
+                build_data.take_snapshot();
+            }
+        }
+
+        // Outline buildings
+        let mut mapclone = build_data.map.clone();
+        for y in 2..build_data.height-2 {
+            for x in 32..build_data.width-2 {
+                let idx = build_data.map.xy_idx(x, y);
+                if build_data.map.tiles[idx] == TileType::WoodFloor {
+                    let mut neighbors = 0;
+                    if build_data.map.tiles[idx - 1] != TileType::WoodFloor { neighbors +=1;}
+                    if build_data.map.tiles[idx + 1] != TileType::WoodFloor { neighbors +=1;}
+                    if build_data.map.tiles[idx-build_data.width as usize] != TileType::WoodFloor { neighbors += 1; }
+                    if build_data.map.tiles[idx+build_data.width as usize] != TileType::WoodFloor { neighbors += 1; }
+                    if neighbors > 0 {
+                        mapclone.tiles[idx] = TileType::Wall;
+                    }
+                }
+            }
+        }
+        build_data.map = mapclone;
+        build_data.take_snapshot();
+        buildings
     }
 }
