9ee99931b762afd08b168583c53980bed5de4bfa
kijimaD <norimaking777@gmail.com>
2022-06-08 10:43:19 +0900

    feat: new combat system

--- src/melee_combat_system.rs
+++ src/melee_combat_system.rs
@@ -1,6 +1,6 @@
 use super::{
-    gamelog::BattleLog, particle_system::ParticleBuilder, Attributes, DefenseBonus, Equipped,
-    HungerClock, HungerState, MeleePowerBonus, Name, Pools, Position, SufferDamage, WantsToMelee,
+    gamelog::BattleLog, particle_system::ParticleBuilder, Attributes, DefenseBonus, Equipped, skill_bonus,
+    HungerClock, HungerState, MeleePowerBonus, Name, Pools, Position, SufferDamage, WantsToMelee, Skills, Skill
 };
 use rltk::RandomNumberGenerator;
 use specs::prelude::*;
@@ -22,6 +22,7 @@ impl<'a> System<'a> for MeleeCombatSystem {
         WriteStorage<'a, WantsToMelee>,
         ReadStorage<'a, Name>,
         ReadStorage<'a, Attributes>,
+        ReadStorage<'a, Skills>,
         ReadStorage<'a, MeleePowerBonus>,
         ReadStorage<'a, DefenseBonus>,
         ReadStorage<'a, Equipped>,
@@ -30,6 +31,7 @@ impl<'a> System<'a> for MeleeCombatSystem {
         ReadStorage<'a, Position>,
         ReadStorage<'a, HungerClock>,
         ReadStorage<'a, Pools>,
+        WriteExpect<'a, rltk::RandomNumberGenerator>
     );
 
     fn run(&mut self, data: Self::SystemData) {
@@ -39,6 +41,7 @@ impl<'a> System<'a> for MeleeCombatSystem {
             mut wants_melee,
             names,
             attributes,
+            skills,
             melee_power_bonuses,
             defense_bonuses,
             equipped,
@@ -47,73 +50,59 @@ impl<'a> System<'a> for MeleeCombatSystem {
             positions,
             hunger_clock,
             pools,
+            mut rng
         ) = data;
 
-        for (entity, wants_melee, name, attacker_pools, attacker_attributes) in
-            (&entities, &wants_melee, &names, &pools, &attributes).join()
+        for (entity, wants_melee, name, attacker_attributes, attacker_skills, attacker_pools) in
+            (&entities, &wants_melee, &names, &attributes, &skills, &pools).join()
         {
-            if attacker_pools.hit_points.current > 0 {
-                let mut offensive_bonus = 0;
-                for (_item_entity, power_bonus, equipped_by) in
-                    (&entities, &melee_power_bonuses, &equipped).join()
-                {
-                    if equipped_by.owner == entity {
-                        offensive_bonus += power_bonus.power;
-                    }
-                }
+            let target_pools = pools.get(wants_melee.target).unwrap();
+            let target_attributes = attributes.get(wants_melee.target).unwrap();
+            let target_skills = skills.get(wants_melee.target).unwrap();
+            if attacker_pools.hit_points.current > 0 && target_pools.hit_points.current > 0 {
+                let target_name = names.get(wants_melee.target).unwrap();
 
-                let hc = hunger_clock.get(entity);
-                if let Some(hc) = hc {
+                let natural_roll = rng.roll_dice(1, 20);
+                let attribute_hit_bonus = attacker_attributes.might.bonus;
+                let skill_hit_bonus = skill_bonus(Skill::Melee, &*attacker_skills);
+                let weapon_hit_bonus = 0;
+                let mut status_hit_bonus = 0;
+
+                if let Some(hc) = hunger_clock.get(entity) {
                     if hc.state == HungerState::WellFed {
-                        offensive_bonus += 1;
+                        status_hit_bonus += 1;
                     }
                 }
+                let modified_hit_roll = natural_roll + attribute_hit_bonus + skill_hit_bonus + weapon_hit_bonus + status_hit_bonus;
 
-                let target_pools = pools.get(wants_melee.target).unwrap();
-                let target_attributes = attributes.get(wants_melee.target).unwrap();
-                if target_pools.hit_points.current > 0 {
-                    let target_name = names.get(wants_melee.target).unwrap();
+                let base_armor_class = 10;
+                let armor_quickness_bonus = target_attributes.quickness.bonus;
+                let armor_skill_bonus = skill_bonus(Skill::Defense, &*target_skills);
+                let armor_item_bonus = 0;
+                let armor_class = base_armor_class + armor_quickness_bonus + armor_skill_bonus + armor_item_bonus;
 
-                    let mut defensive_bonus = 0;
-                    for (_item_entity, defense_bonus, equipped_by) in
-                        (&entities, &defense_bonuses, &equipped).join()
-                    {
-                        if equipped_by.owner == wants_melee.target {
-                            defensive_bonus += defense_bonus.defense;
-                        }
+                if natural_roll != 1 && (natural_roll == 20 || modified_hit_roll > armor_class) {
+                    let base_damage = rng.roll_dice(1, 4);
+                    let attr_damage_bonus = attacker_attributes.might.bonus;
+                    let skill_damage_bonus = skill_bonus(Skill::Melee, &*attacker_skills);
+                    let weapon_damage_bonus = 0;
+
+                    let damage = i32::max(0, base_damage + attr_damage_bonus + skill_hit_bonus + skill_damage_bonus + weapon_damage_bonus);
+                    SufferDamage::new_damage(&mut inflict_damage, wants_melee.target, damage);
+                    log.entries.push(format!("{} hits {}, for {} hp.", &name.name, &target_name.name, damage));
+                    if let Some(pos) = positions.get(wants_melee.target) {
+                        // TODO: 戦闘モードに対応させる
+                        particle_builder.request(pos.x, pos.y, rltk::RGB::named(rltk::ORANGE), rltk::RGB::named(rltk::BLACK), rltk::to_cp437('‼'), 200.0);
                     }
-
-                    // TODO: 戦闘モードに対応させる
-                    let pos = positions.get(wants_melee.target);
-                    if let Some(pos) = pos {
-                        particle_builder.request(
-                            pos.x,
-                            pos.y,
-                            rltk::RGB::named(rltk::ORANGE),
-                            rltk::RGB::named(rltk::BLACK),
-                            rltk::to_cp437('‼'),
-                            200.0,
-                        );
+                } else if natural_roll == 1 {
+                    log.entries.push(format!("{} consider attacking {}, but misjudges the timing.", name.name, target_name.name));
+                    if let Some(pos) = positions.get(wants_melee.target) {
+                        particle_builder.request(pos.x, pos.y, rltk::RGB::named(rltk::BLUE), rltk::RGB::named(rltk::BLACK), rltk::to_cp437('‼'), 200.0);
                     }
-
-                    let mut rng = RandomNumberGenerator::new();
-                    let damage = i32::max(
-                        0,
-                        (attacker_attributes.might.bonus + offensive_bonus)
-                            - (target_attributes.might.bonus + defensive_bonus),
-                    ) + rng.range(1, 5);
-
-                    if damage == 0 {
-                        log.entries.push(format!(
-                            "{} is unable to hurt {}",
-                            &name.name, &target_name.name
-                        ));
-                    } else {
-                        log.entries.push(format!(
-                            "{} hits {}, for {} hp",
-                            &name.name, &target_name.name, damage
-                        ));
-                        SufferDamage::new_damage(&mut inflict_damage, wants_melee.target, damage);
+                } else{
+                    log.entries.push(format!("{} attacks {}, but can't connect.", name.name, target_name.name));
+                    if let Some(pos) = positions.get(wants_melee.target) {
+                        particle_builder.request(pos.x, pos.y, rltk::RGB::named(rltk::CYAN), rltk::RGB::named(rltk::BLACK), rltk::to_cp437('‼'), 200.0);
                     }
                 }
             }
