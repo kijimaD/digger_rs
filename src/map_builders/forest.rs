07f481544de91aef85f3bba8f258a8b5ee47a9f8
kijimaD <norimaking777@gmail.com>
2022-06-12 14:27:25 +0900

    feat: init forest builder

---
+++ src/map_builders/forest.rs
@@ -0,0 +1,14 @@
+use super::{BuilderChain, CellularAutomataBuilder, XStart, YStart, AreaStartingPosition, CullUnreachable, VoronoiSpawning, DistantExit};
+
+pub fn forest_builder(new_depth: i32, _rng: &mut rltk::RandomNumberGenerator, width: i32, height: i32) -> BuilderChain {
+    let mut chain = BuilderChain::new(new_depth, width, height, "Into the Woods");
+    chain.start_with(CellularAutomataBuilder::new());
+    chain.with(AreaStartingPosition::new(XStart::CENTER, YStart::CENTER));
+    chain.with(CullUnreachable::new());
+    chain.with(AreaStartingPosition::new(XStart::LEFT, YStart::CENTER));
+
+    // Setup an exit and spawn mobs
+    chain.with(VoronoiSpawning::new());
+    chain.with(DistantExit::new());
+    chain
+}
