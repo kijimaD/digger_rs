a3ad05dddedda14b382b9dbc05055fea589aa0c7
kijimaD <norimaking777@gmail.com>
2022-06-12 14:57:10 +0900

    refactor: divide camera and theme

--- src/map_builders/forest.rs
+++ src/map_builders/forest.rs
@@ -1,6 +1,14 @@
-use super::{BuilderChain, CellularAutomataBuilder, XStart, YStart, AreaStartingPosition, CullUnreachable, VoronoiSpawning, DistantExit};
+use super::{
+    AreaStartingPosition, BuilderChain, CellularAutomataBuilder, CullUnreachable, DistantExit,
+    VoronoiSpawning, XStart, YStart,
+};
 
-pub fn forest_builder(new_depth: i32, _rng: &mut rltk::RandomNumberGenerator, width: i32, height: i32) -> BuilderChain {
+pub fn forest_builder(
+    new_depth: i32,
+    _rng: &mut rltk::RandomNumberGenerator,
+    width: i32,
+    height: i32,
+) -> BuilderChain {
     let mut chain = BuilderChain::new(new_depth, width, height, "Into the Woods");
     chain.start_with(CellularAutomataBuilder::new());
     chain.with(AreaStartingPosition::new(XStart::CENTER, YStart::CENTER));
