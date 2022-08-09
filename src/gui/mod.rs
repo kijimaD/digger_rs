34668556ebc542fa140395bb9285845597382b69
kijimaD <norimaking777@gmail.com>
2022-08-09 23:44:17 +0900

    refactor: use rltk's draw_hollow_box

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -9,14 +9,15 @@ use specs::prelude::*;
 pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
     use rltk::to_cp437;
     let box_gray: RGB = RGB::from_hex("#999999").expect("Oops");
-    let black = RGB::named(rltk::BLACK);
-    let white = RGB::named(rltk::WHITE);
+    let gray = RGB::named(rltk::GRAY).to_rgba(1.0);
+    let black = RGB::named(rltk::BLACK).to_rgba(1.0);
+    let white = RGB::named(rltk::WHITE).to_rgba(1.0);
 
     // separators
-    draw_hollow_box(ctx, 0, 0, 79, 59, box_gray, black); // Overall box
-    draw_hollow_box(ctx, 0, 0, 49, 45, box_gray, black); // Map box
-    draw_hollow_box(ctx, 0, 45, 79, 14, box_gray, black); // Log box
-    draw_hollow_box(ctx, 49, 0, 30, 8, box_gray, black); // Top-right panel
+    ctx.draw_hollow_box(0, 0, 79, 59, gray, black); // Overall box
+    ctx.draw_hollow_box(0, 0, 49, 45, gray, black); // Map box
+    ctx.draw_hollow_box(0, 45, 79, 14, gray, black); // Log box
+    ctx.draw_hollow_box(49, 0, 30, 8, gray, black); // Top-right panel
 
     // connectors
     ctx.set(0, 45, box_gray, black, to_cp437('├'));
@@ -160,32 +161,6 @@ pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
 
     // Tooltip
     draw_tooltips(ecs, ctx);
-}
-
-pub fn draw_hollow_box(
-    console: &mut Rltk,
-    sx: i32,
-    sy: i32,
-    width: i32,
-    height: i32,
-    fg: RGB,
-    bg: RGB,
-) {
-    use rltk::to_cp437;
-
-    console.set(sx, sy, fg, bg, to_cp437('┌'));
-    console.set(sx + width, sy, fg, bg, to_cp437('┐'));
-    console.set(sx, sy + height, fg, bg, to_cp437('└'));
-    console.set(sx + width, sy + height, fg, bg, to_cp437('┘'));
-    for x in sx + 1..sx + width {
-        console.set(x, sy, fg, bg, to_cp437('─'));
-        console.set(x, sy + height, fg, bg, to_cp437('─'));
-    }
-
-    for y in sy + 1..sy + height {
-        console.set(sx, y, fg, bg, to_cp437('│'));
-        console.set(sx + width, y, fg, bg, to_cp437('│'));
-    }
 }
 
 fn draw_attribute(name: &str, attribute: &Attribute, y: i32, ctx: &mut Rltk) {
