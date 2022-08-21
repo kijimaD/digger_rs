d30ce01317b3e4c68f8740c100919265ba0fdc46
kijimaD <norimaking777@gmail.com>
2022-08-21 16:49:53 +0900

    refactor: explicit module

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,17 @@ mod damage_system;
 pub mod effects;
 mod encounter_system;
 mod gamelog;
-mod gui;
-mod run_away_system;
-mod spawner;
-mod systems;
-use systems::{
-    cull_dead_particles, BattleActionSystem, HungerSystem, LightingSystem, MapIndexingSystem,
-    MeleeCombatSystem, MovementSystem, ParticleBuilder, ParticleSpawnSystem, TriggerSystem,
-    VisibilitySystem,
-};
-mod inventory_system;
-use inventory_system::{
-    ItemCollectionSystem, ItemDropSystem, ItemEquipOnUse, ItemRemoveSystem, ItemUseSystem,
-};
 mod gamesystem;
+mod gui;
+mod inventory_system;
 pub mod map_builders;
 pub mod random_table;
 pub mod raws;
 pub mod rex_assets;
+mod run_away_system;
 pub mod saveload_system;
+mod spawner;
+mod systems;
 pub use gamesystem::*;
 mod spatial;
 
@@ -88,13 +80,13 @@ pub struct State {
 
 impl State {
     fn run_field_systems(&mut self) {
-        let mut vis = VisibilitySystem {};
+        let mut vis = systems::VisibilitySystem {};
         vis.run_now(&self.ecs);
         let mut initiative = ai::InitiativeSystem {};
         initiative.run_now(&self.ecs);
         let mut quipper = ai::QuipSystem {};
         quipper.run_now(&self.ecs);
-        let mut moving = MovementSystem {};
+        let mut moving = systems::MovementSystem {};
         moving.run_now(&self.ecs);
         let mut visible = ai::VisibleAI {};
         visible.run_now(&self.ecs);
@@ -106,40 +98,40 @@ impl State {
         flee.run_now(&self.ecs);
         let mut defaultmove = ai::DefaultMoveAI {};
         defaultmove.run_now(&self.ecs);
-        let mut mapindex = MapIndexingSystem {};
+        let mut mapindex = systems::MapIndexingSystem {};
         mapindex.run_now(&self.ecs);
-        let mut pickup = ItemCollectionSystem {};
+        let mut pickup = inventory_system::ItemCollectionSystem {};
         pickup.run_now(&self.ecs);
-        let mut itemequip = ItemEquipOnUse {};
+        let mut itemequip = inventory_system::ItemEquipOnUse {};
         itemequip.run_now(&self.ecs);
-        let mut itemuse = ItemUseSystem {};
+        let mut itemuse = inventory_system::ItemUseSystem {};
         itemuse.run_now(&self.ecs);
-        let mut drop_items = ItemDropSystem {};
+        let mut drop_items = inventory_system::ItemDropSystem {};
         drop_items.run_now(&self.ecs);
-        let mut item_remove = ItemRemoveSystem {};
+        let mut item_remove = inventory_system::ItemRemoveSystem {};
         item_remove.run_now(&self.ecs);
-        let mut hunger = HungerSystem {};
+        let mut hunger = systems::HungerSystem {};
         hunger.run_now(&self.ecs);
         let mut encumbrance = ai::EncumbranceSystem {};
         encumbrance.run_now(&self.ecs);
-        let mut trigger = TriggerSystem {};
+        let mut trigger = systems::TriggerSystem {};
         trigger.run_now(&self.ecs);
 
         effects::run_effects_queue(&mut self.ecs);
-        let mut particles = ParticleSpawnSystem {};
+        let mut particles = systems::ParticleSpawnSystem {};
         particles.run_now(&self.ecs);
-        let mut lighting = LightingSystem {};
+        let mut lighting = systems::LightingSystem {};
         lighting.run_now(&self.ecs);
 
         self.ecs.maintain();
     }
 
     fn run_battle_systems(&mut self) {
-        let mut battle_action = BattleActionSystem {};
+        let mut battle_action = systems::BattleActionSystem {};
         battle_action.run_now(&self.ecs);
-        let mut melee = MeleeCombatSystem {};
+        let mut melee = systems::MeleeCombatSystem {};
         melee.run_now(&self.ecs);
-        let mut itemuse = ItemUseSystem {};
+        let mut itemuse = inventory_system::ItemUseSystem {};
         itemuse.run_now(&self.ecs);
 
         effects::run_effects_queue(&mut self.ecs);
@@ -160,7 +152,7 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(0);
         ctx.cls();
-        cull_dead_particles(&mut self.ecs, ctx);
+        systems::cull_dead_particles(&mut self.ecs, ctx);
 
         // マップUI表示
         match newrunstate {
@@ -723,7 +715,7 @@ fn main() -> rltk::BError {
     gs.ecs.insert(player_entity);
     gs.ecs.insert(RunState::MapGeneration {});
     gamelog::clear_log(&crate::gamelog::FIELD_LOG);
-    gs.ecs.insert(ParticleBuilder::new());
+    gs.ecs.insert(systems::ParticleBuilder::new());
     gs.ecs.insert(rex_assets::RexAssets::new());
 
     gs.generate_world_map(1, 0);
