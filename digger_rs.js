150225e2182d1ec4ea5badf9f2291ed626d542fe
kijimaD <norimaking777@gmail.com>
2022-08-22 08:44:44 +0000

    Deploying to gh-pages from @ kijimaD/digger_rs@8703f3e2484488a5028d25b43e0b850056aa4edd 🚀

--- digger_rs.js
+++ digger_rs.js
@@ -786,15 +786,15 @@ function getImports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1844 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 408, __wbg_adapter_24);
+    imports.wbg.__wbindgen_closure_wrapper1711 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 320, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2227 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2220 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 649, __wbg_adapter_27);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2229 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2222 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 649, __wbg_adapter_27);
         return addHeapObject(ret);
     };
