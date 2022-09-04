e8bb64be7431066d9cd597f48ff6af94703d3098
kijimaD <norimaking777@gmail.com>
2022-09-04 16:45:15 +0900

    feat: inventory and equipment menu

--- src/gui/equip_item_menu.rs
+++ src/gui/equip_item_menu.rs
@@ -1,4 +1,7 @@
-use super::{item_result_menu, Equippable, InBackpack, ItemMenuResult, Name, State};
+use super::{
+    item_result_menu, Combatant, Equippable, Equipped, InBackpack, ItemMenuResult, Name, Player,
+    State,
+};
 use rltk::prelude::*;
 use specs::prelude::*;
 
@@ -6,34 +9,39 @@ pub fn equip_item_menu(gs: &mut State, ctx: &mut Rltk) -> (ItemMenuResult, Optio
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
 
-    // 右側の装備スロット
-    draw_batch.draw_box(
-        Rect::with_size(50, 15, 20, 4),
-        ColorPair::new(RGB::named(rltk::WHITE), RGB::named(rltk::BLACK)),
-    );
-
-    // 左側のインベントリ
     let player_entity = gs.ecs.fetch::<Entity>();
+    let players = gs.ecs.read_storage::<Player>();
+    let combatants = gs.ecs.read_storage::<Combatant>();
     let names = gs.ecs.read_storage::<Name>();
-    let backpack = gs.ecs.read_storage::<InBackpack>();
-    let equippables = gs.ecs.read_storage::<Equippable>();
+    let equipped = gs.ecs.read_storage::<Equipped>();
     let entities = gs.ecs.entities();
 
+    // 右側の装備スロット
     let mut items: Vec<(Entity, String)> = Vec::new();
-    (&entities, &backpack).join().filter(|item| item.1.owner == *player_entity).for_each(|item| {
-        let name = names.get(item.0).unwrap();
-        items.push((item.0, name.name.clone()))
-    });
+    // TODO: まだパーティ最初の一人固定なので選択できるようにする
+    for (entity, _combatant, _player) in (&entities, &combatants, &players).join().take(1) {
+        (&entities, &equipped).join().filter(|item| item.1.owner == entity).for_each(|item| {
+            let name = names.get(item.0).unwrap();
+            items.push((item.0, name.name.clone()))
+        });
+    }
+    item_result_menu(&mut draw_batch, "Equipped", items.len(), &items, ctx.key, Some(40), None);
 
-    let result = item_result_menu(
-        &mut draw_batch,
-        "Equip which item?",
-        items.len(),
-        &items,
-        ctx.key,
-        None,
-        None,
-    );
+    // 左側のインベントリ
+    let backpack = gs.ecs.read_storage::<InBackpack>();
+    let equippables = gs.ecs.read_storage::<Equippable>();
+
+    let mut items: Vec<(Entity, String)> = Vec::new();
+    (&entities, &backpack, &equippables)
+        .join()
+        .filter(|item| item.1.owner == *player_entity)
+        .for_each(|item| {
+            let name = names.get(item.0).unwrap();
+            items.push((item.0, name.name.clone()))
+        });
+
+    let result =
+        item_result_menu(&mut draw_batch, "Inventory", items.len(), &items, ctx.key, Some(4), None);
     draw_batch.submit(6000);
     result
 }
