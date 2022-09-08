cc51235a76fd3a183aa240c9d03b14e0211dde40
kijimaD <norimaking777@gmail.com>
2022-09-08 22:07:20 +0900

    chore: player1, player2

--- src/main.rs
+++ src/main.rs
@@ -825,8 +825,8 @@ fn main() -> rltk::BError {
     gs.ecs.insert(Point::new(0, 0));
     gs.ecs.insert(rltk::RandomNumberGenerator::new());
 
-    let battle_entity1 = spawner::battle_player(&mut gs.ecs);
-    let battle_entity2 = spawner::battle_player(&mut gs.ecs);
+    let battle_entity1 = spawner::battle_player(&mut gs.ecs, "player1".to_string());
+    let battle_entity2 = spawner::battle_player(&mut gs.ecs, "player2".to_string());
     let field_entity = spawner::player(&mut gs.ecs, 0, 0);
     gs.ecs.insert(battle_entity1);
     gs.ecs.insert(battle_entity2);
